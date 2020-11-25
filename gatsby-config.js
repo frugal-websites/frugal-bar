2d5244bda2e6d271e33169e899e0c30c3c65ab31
JeHuppe <jeremie.huppe@gmail.com>
2020-11-25 11:46:41 -0500

    Connect to MongoDB with Realm GQL API

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,35 @@
 require("dotenv").config({
   path: `.env.${process.env.NODE_ENV}`,
 })
+Realm = require("realm-web")
+
+const app = new Realm.App(process.env.REALM_APP_ID)
+
+// Get a valid Realm user access token to authenticate requests
+async function getValidAccessToken(app) {
+  if (!app.currentUser) {
+    // If no user is logged in, log in an anonymous user
+    console.log(`1`)
+
+    await app.logIn(Realm.Credentials.anonymous())
+    console.log(`2`)
+  } else {
+    // The logged in user's access token might be stale,
+    // Refreshing custom data also refreshes the access token
+    console.log(`3`)
+    await app.currentUser.refreshCustomData()
+    console.log(`4`)
+  }
+  // Get a valid access token for the current user
+  console.log(`5`)
+  return app.currentUser.accessToken
+}
+
+async function getAuthorizationToken(app) {
+  const accessToken = await getValidAccessToken(app)
+
+  return `Bearer ${accessToken}`
+}
 
 module.exports = {
   siteMetadata: {
@@ -36,12 +65,35 @@ module.exports = {
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
+    // {
+    //   resolve: `gatsby-source-mongodb`,
+    //   options: {
+    //     dbName: process.env.MONGODB_DB_NAME,
+    //     collection: process.env.MONGODB_COLLECTION_NAME,
+    //     connectionString: process.env.MONGODB_CONNECTION_STRING,
+    //   },
+    // },
+
+    // REALM_GQL_TOKEN=JkS4PbIuf1ELHnXEIAK4wKhWW9Q9AV80bNbyv8ZNhhq0dZghD9zRQyM0Nwvg4mFB
+    // REALM_GQL_URL=https://us-east-1.aws.realm.mongodb.com/api/client/v2.0/app/application-frugal-zdrdn/graphql
     {
-      resolve: `gatsby-source-mongodb`,
+      resolve: "gatsby-source-graphql",
       options: {
-        dbName: process.env.MONGODB_DB_NAME,
-        collection: process.env.MONGODB_COLLECTION_NAME,
-        connectionString: process.env.MONGODB_CONNECTION_STRING,
+        typeName: "Frugal", // This is arbitrary, see https://github.com/gatsbyjs/gatsby/issues/18877.
+        fieldName: "mongodbGqlApi", // This is arbitrary, see https://github.com/gatsbyjs/gatsby/issues/18877.
+        url: process.env.REALM_GQL_URL,
+        // HTTP headers
+        // headers: {
+        //   // apiKey: process.env.REALM_GQL_URL,
+        //   Authorization: `apiKey ${process.env.REALM_GQL_API_KEY}`,
+
+        //   // Authorization: `Bearer ${process.env.REALM_GQL_TOKEN}`,
+        // },
+        headers: async () => {
+          return {
+            Authorization: await getAuthorizationToken(app),
+          }
+        },
       },
     },
     {
